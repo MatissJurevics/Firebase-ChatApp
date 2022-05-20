f01385ddbd4346cd550e44a5d09cdb1d0068a186
matiss <matissjurevics@gmail.com>
2022-05-20 21:36:51 +0100

    got the messages to work (not connected to db)

---
+++ src/components/InMessage.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { auth } from "../firebase"
+
+const props = defineProps({
+  message: String,
+  author: String,
+  id: String,
+});
+</script>
+
+<template>
+
+  <li v-if="author !== auth.currentUser.displayName" class="p-2 flex flex-row items-center w-full min-h-8 hover:bg-white/30">
+    <div class="flex flex-col">
+        <span class="text-black/50 text-sm mb-1">{{ author }}</span>
+        <span class="max-w-[250px] p-1 px-2 bg-cyan-300 text-white rounded-xl">
+            {{message}}
+        </span>
+    </div>
+  </li>
+   <li v-else class="p-2 flex flex-row justify-between items-center w-full min-h-8 hover:bg-white/30">
+       <div>
+        I
+    </div>
+    <div class="flex flex-col items-end">
+        <span class="text-black/50 text-sm mb-1">{{ author }}</span>
+        <span class="max-w-[250px] p-1 px-2 bg-green-300 text-white rounded-xl">
+            {{message}}
+        </span>
+    </div>
+
+  </li>
+</template>
--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -1,10 +1,20 @@
 <script setup>
 import { getDoc, collection } from "firebase/firestore"
+import { db } from "../firebase"
 
+import Message from "./InMessage.vue"
+
+// const Messsages = async () => {
+//     await getDoc(collection(db, "messsages"))
+// }
 
 </script>
 
 <template>
-  <div class="w-full h-4/5">
+
+  <div class="w-full h-4/5 overflow-y-auto">
+  <Message message="This is a sample message written by me" author="Matiss Jurevics"/>
+  <Message message="This is a sample message written by me" author="Matis Jurevics"/>
+
   </div>
 </template>
