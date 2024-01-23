987bfa594d67685f5587ad4fe3ca1475e526d5ca
matiss <matissjurevics@gmail.com>
2024-01-23 16:45:22 +0000

    fixed

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -6,9 +6,10 @@ import { ref } from 'vue'
 import Message from "./InMessage.vue"
 
 const messages = ref()
-const q = query(collection(db, "messages"), orderBy("Created"), limit(25))
+const q = query(collection(db, "messages"), orderBy("Created", "desc"), limit(50))
 const subscribe = onSnapshot(q, docs => {
   let posts = []
+  
   docs.forEach(doc => {
     let docdata = {
       text: doc.data().message,
@@ -21,15 +22,18 @@ const subscribe = onSnapshot(q, docs => {
 
   })
   console.log(posts)
-  messages.value = posts
+  messages.value = posts.reverse()
 })
+
+
+
 
 
 </script>
 
 <template>
 
-  <div class="w-full h-4/5 overflow-y-auto">
+  <div id="messageBody" class="w-full h-4/5 overflow-y-auto">
     <Message v-for="message in messages" :text="message.text" :author="message.user" :uid="message.uid" :imgURL="message.uimg" :id="message.id" :key="message.id" />
   </div>
 </template>
